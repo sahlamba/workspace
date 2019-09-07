7e8cf7a2f8d5cf17b9b54192c224b8ca916e7855
sahlamba <sahil.lamba95@gmail.com>
2019-09-07 21:39:20 +0530

    feat: add Google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import 'draft-js/dist/Draft.css'
 import './css/App.scss'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Header } from './components/Header'
 import { NoteEditor } from './components/NoteEditor'
 import { WorkspaceContextProvider } from './context/WorkspaceContext'
 import { DarkModeContextProvider } from './context/DarkModeContext'
 
+import ReactGA from 'react-ga'
+ReactGA.initialize('UA-147259310-1')
+
 export const App = () => {
+  useEffect(() => {
+    ReactGA.pageview('/')
+  }, [])
+
   return (
     <DarkModeContextProvider>
       <WorkspaceContextProvider>
